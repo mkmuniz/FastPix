495358b992a9d44335b2c3ee8e38a63cbe8ed3f6
Mkmuniz <mikael.muniz@bolha.com.br>
2024-12-10 14:45:36 -0300

    fix: qrcode pix to dynamic

--- back/src/main/java/io/github/mkmuniz/fast_pix/core/service/QrCodeService.java
+++ back/src/main/java/io/github/mkmuniz/fast_pix/core/service/QrCodeService.java
@@ -20,69 +20,70 @@ public class QrCodeService implements QrCodeServicePort {
     public QrCode generateQrCode(Pix pix) {
         String pixCode = generatePixQrCodeText(pix);
         String qrCodeImage = generateQrCodeImage(pixCode);
-        
+
         return new QrCode.Builder()
-            .withText(pixCode)
-            .withImage(qrCodeImage)
-            .build();
+                .withText(pixCode)
+                .withImage(qrCodeImage)
+                .build();
     }
 
     private String generatePixQrCodeText(Pix pix) {
         StringBuilder sb = new StringBuilder();
-        
+
         // Payload Format Indicator
         sb.append("000201");
-        
+
         // Merchant Account Information
         sb.append("26");
         String merchantAccountInfo = String.format(
-            "0014BR.GOV.BCB.PIX01%02d%s",
-            pix.getPixKey().length(),
-            pix.getPixKey()
-        );
+                "0014BR.GOV.BCB.PIX01%02d%s",
+                pix.getPixKey().length(),
+                pix.getPixKey());
         sb.append(String.format("%02d%s", merchantAccountInfo.length(), merchantAccountInfo));
-        
+
         // Merchant Category Code
         sb.append("52040000");
-        
+
         // Transaction Currency
         sb.append("5303986");
-        
+
         // Transaction Amount
         DecimalFormat df = new DecimalFormat("0.00");
         String amount = df.format(pix.getValue());
         sb.append(String.format("54%02d%s", amount.length(), amount));
-        
+
         // Country Code
         sb.append("5802BR");
-        
+
         // Beneficiary Name
         String receiverName = removeAccents(pix.getName().toUpperCase());
         if (receiverName.length() > 25) {
             receiverName = receiverName.substring(0, 25);
         }
         sb.append(String.format("59%02d%s", receiverName.length(), receiverName));
-        
+
         // City
         String city = removeAccents(pix.getCity().toUpperCase());
         if (city.length() > 15) {
             city = city.substring(0, 15);
         }
         sb.append(String.format("60%02d%s", city.length(), city));
-        
-        // Additional Data Field Template (Campo 62)
+
+        // Additional Data Field Template (Campo 62) for dynamic QR
         String txId = generateTransactionId();
-        sb.append("62")
-          .append(String.format("%02d", txId.length() + 4))
-          .append("05")
-          .append(String.format("%02d", txId.length()))
-          .append(txId);
-        
+        sb.append("62");
+        String gui = "br.gov.bcb.pix";
+        String additionalData = String.format(
+                "01%02d%s05%02d%s",
+                gui.length(), gui,
+                txId.length(), txId);
+        sb.append(String.format("%02d%s", additionalData.length(), additionalData));
+
         // CRC16
         sb.append("6304");
         String crc16 = calculateCRC16(sb.toString());
         sb.append(crc16);
-        
+
         return sb.toString();
     }
 
@@ -90,21 +91,21 @@ public class QrCodeService implements QrCodeServicePort {
         StringBuilder txId = new StringBuilder();
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         int length = 25;
-        
+
         java.security.SecureRandom random = new java.security.SecureRandom();
-        
+
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(chars.length());
             txId.append(chars.charAt(index));
         }
-        
+
         return txId.toString();
     }
 
     private String calculateCRC16(String qrCode) {
         int polynom = 0x1021;
         int crc = 0xFFFF;
-        
+
         for (int i = 0; i < qrCode.length(); i++) {
             crc ^= (qrCode.charAt(i) << 8);
             for (int j = 0; j < 8; j++) {
@@ -115,7 +116,7 @@ public class QrCodeService implements QrCodeServicePort {
                 }
             }
         }
-        
+
         return String.format("%04X", crc & 0xFFFF).toUpperCase();
     }
 
@@ -123,10 +124,10 @@ public class QrCodeService implements QrCodeServicePort {
         try {
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
             BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, 300, 300);
-            
+
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
-            
+
             return "data:image/png;base64," + Base64.getEncoder().encodeToString(outputStream.toByteArray());
         } catch (Exception e) {
             throw new RuntimeException("Erro ao gerar imagem do QR Code", e);
@@ -135,6 +136,6 @@ public class QrCodeService implements QrCodeServicePort {
 
     private String removeAccents(String input) {
         return Normalizer.normalize(input, Normalizer.Form.NFD)
-                         .replaceAll("[^\\p{ASCII}]", "");
+                .replaceAll("[^\\p{ASCII}]", "");
     }
 }
