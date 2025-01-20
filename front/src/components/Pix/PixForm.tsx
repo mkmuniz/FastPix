1b71071be9289abf68df0fa6f404cb685dfbf29b
Mkmuniz <mikael.muniz@bolha.com.br>
2025-01-19 21:23:29 -0300

    fix: regex for phone

--- front/src/components/Pix/PixForm.tsx
+++ front/src/components/Pix/PixForm.tsx
@@ -26,7 +26,7 @@ export default function PixForm({ onQrCodeGenerated }: PixFormProps) {
       case 'email':
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
       case 'phone':
-        return /^\d{11}$/.test(value);
+        return /^(\(\d{2}\)\s?|\d{2})\s?\d{5}[-\s]?\d{4}$/.test(value);
       case 'document':
         return /^\d{11}$|^\d{14}$/.test(value);
       default:
